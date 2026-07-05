de490e2067b63a32be879701be3c2625f9f9c8fc
Subash-G-S <subashgopi2006@gmail.com>
2026-07-05 23:47:24 +0530

    converted to resend email

--- app/services/mail_service.py
+++ app/services/mail_service.py
@@ -1,45 +1,29 @@
 import os
 
+import resend
 from dotenv import load_dotenv
-from fastapi_mail import ConnectionConfig, FastMail, MessageSchema
 
 load_dotenv()
 
-conf = ConnectionConfig(
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=int(os.getenv("MAIL_PORT")),
-    MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_STARTTLS=True,
-    MAIL_SSL_TLS=False,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True,
-)
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 
-async def send_otp_email(
+def send_otp_email(
     email: str,
     otp: str,
 ):
     try:
-        message = MessageSchema(
-            subject="AMIGO Password Reset OTP",
-            recipients=[email],
-            body=f"Your AMIGO OTP is: {otp}",
-            subtype="plain",
+        resend.Emails.send(
+            {
+                "from": "Amigo <onboarding@resend.dev>",
+                "to": [email],
+                "subject": "AMIGO Password Reset OTP",
+                "text": f"Your OTP is: {otp}",
+            }
         )
-
-        fm = FastMail(conf)
-
-        print("Sending OTP:", otp)
-
-        await fm.send_message(message)
 
         print("Email successfully sent")
 
     except Exception as e:
-        import traceback
-
-        print("MAIL ERROR:", str(e))
-        traceback.print_exc()
+        print("Resend Error:", e)
+        raise
