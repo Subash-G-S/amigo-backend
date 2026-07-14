99ba031064d965e36a762c076f9522c99050f55b
Subash-G-S <subashgopi2006@gmail.com>
2026-07-14 17:23:53 +0530

    Fixed the loadmypost for profile page

--- app/routes/post.py
+++ app/routes/post.py
@@ -9,6 +9,7 @@ from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
+from app.models.comment import Comment
 from app.models.like import Like
 from app.models.post import Post
 from app.models.user import User
@@ -122,7 +123,7 @@ def get_my_posts(
             algorithms=[ALGORITHM],
         )
 
-        user_id = payload["sub"]
+        current_user = payload["sub"]
 
     except JWTError:
         raise HTTPException(
@@ -131,10 +132,44 @@ def get_my_posts(
         )
 
     posts = (
-        db.query(Post)
-        .filter(Post.user_id == user_id)
+        db.query(Post, User)
+        .join(User, User.id == Post.user_id)
+        .filter(Post.user_id == current_user)
         .order_by(Post.created_at.desc())
         .all()
     )
+    my_posts = []
+
+    for post, user in posts:
+        like_count = (
+            db.query(func.count(Like.id)).filter(Like.post_id == post.id).scalar()
+        )
+
+        liked = (
+            db.query(Like)
+            .filter(
+                Like.post_id == post.id,
+                Like.user_id == current_user,
+            )
+            .first()
+            is not None
+        )
+
+        comment_count = (
+            db.query(func.count(Comment.id)).filter(Comment.post_id == post.id).scalar()
+        )
+
+        my_posts.append(
+            {
+                "id": post.id,
+                "author": user.name,
+                "content": post.content,
+                "created_at": post.created_at,
+                "likes": like_count,
+                "comments": comment_count,
+                "liked": liked,
+                "is_anonymous": post.is_anonymous,
+            }
+        )
 
     return posts
